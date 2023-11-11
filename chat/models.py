b703829b103d145927f81a690ae1319a2ba64ef4
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2023-11-11 00:46:19 -0300

    refactor: ajuste no model usuário

--- chat/models.py
+++ chat/models.py
@@ -1,1 +1,13 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+
+User._meta.get_field("email")._unique = True
+User.USERNAME_FIELD = "email"
+User.REQUIRED_FIELDS = ["username"]
+
+
+@receiver(pre_save, sender=User)
+def preencher_username_com_email(sender, instance: User, *args, **kwargs):
+    instance.username = instance.email
