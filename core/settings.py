d8e56da96830eb22a818bbfabe0cb9931fee6b4c
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2023-11-11 02:15:47 -0300

    refactor: ajuste para rodar no docker compose e .env.template

--- core/settings.py
+++ core/settings.py
@@ -148,7 +148,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("chat_redis", 6379)],
         },
     },
 }
